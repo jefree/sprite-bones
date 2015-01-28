f50075a5297c2cb3755e34dd37d051e4db5e58d3
jefree <jgarzon920429@gmail.com>
2015-01-28 16:25:44 -0500

    add basic play animation

--- example/js/example.js
+++ example/js/example.js
@@ -5,11 +5,17 @@ var Example = function(game) {}
 Example.prototype = {
 
   preload: function() {
+    //active the plugin
     game.plugins.add(Phaser.Plugin.SpriteBones);
+
+    //load assets
+    game.spritebones.loadAssets('moto', 'example/assets/moto_atlas.json', 'example/assets/moto_texture.png', 'example/assets/moto_skeleton.json')
   },
 
   create: function() {
 
+    var moto = game.spritebones.addSprite('moto', 'motorcycleMan', 'Motorcycle', 100, 100);
+    moto.gotoAndPlay('left', 0.2);
   },
 
   update: function() {
--- src/sprite_bones.js
+++ src/sprite_bones.js
@@ -1,11 +1,49 @@
 Phaser.Plugin.SpriteBones = function(game, parent) {
   Phaser.Plugin.call(this, game, parent);
+  
   dragonBones.game = game;
+  game.spritebones = this;
 }
 
 Phaser.Plugin.SpriteBones.prototype = Object.create(Phaser.Plugin.prototype);
 Phaser.Plugin.SpriteBones.prototype.constructor = Phaser.Plugin.SpriteBones;
 
+Phaser.Plugin.SpriteBones.prototype.loadAssets = function(key, atlas, image, skeleton) {
+  this.game.load.image(key + "_image", image);
+  this.game.load.json(key + "_atlas", atlas);
+  
+  this.game.load.atlas(key, image, atlas);  
+  this.game.load.json(key + "_skeleton", skeleton);
+}
+
+Phaser.Plugin.SpriteBones.prototype.addSprite = function(key, armatureName, skeletonName, x, y) {
+  var skeletonData = this.game.cache.getJSON(key + '_skeleton');
+  var atlasData = this.game.cache.getJSON(key + '_atlas');
+  var texture = game.cache.getImage(key + "_image");
+
+  var armature = dragonBones.makePhaserArmature(armatureName, skeletonName, '',  skeletonData, atlasData, texture, [], key);
+  var bones = armature.getDisplay();
+
+  dragonBones.animation.WorldClock.clock.add(armature);
+
+  this._extendSprite(bones, armature);
+
+  bones.x = x;
+  bones.y = y;
+
+  return this.game.world.add(bones);
+}
+
+Phaser.Plugin.SpriteBones.prototype._extendSprite = function(sprite, armature) {
+
+  sprite._armature = armature;
+
+  sprite.gotoAndPlay = function(animationName, fadeInTime, duration, loop, layer, group, fadeOutMode, displayControl, pauseFadeOut, pauseFadeIn) {
+    this._armature.animation.gotoAndPlay(animationName, fadeInTime, duration, loop, layer, group, fadeOutMode, displayControl, pauseFadeOut, pauseFadeIn);
+  }
+
+}
+
 Phaser.Plugin.SpriteBones.prototype.update = function() {
-  console.log('SpriteBones running');
+  dragonBones.animation.WorldClock.clock.advanceTime(0.02);
 }
